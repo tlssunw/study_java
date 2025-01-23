f5a8daccc95aaa886e6811df8931cd66efddda73
tlssunw <sunw1044@naver.com>
2025-01-23 12:47:09 +0900

    비트 논리 연산자 - 24(금) 이어서 계속

--- java_study1/src/java_study0123/java0123_2.java
+++ java_study1/src/java_study0123/java0123_2.java
@@ -3,7 +3,10 @@ package java_study0123;
 public class java0123_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//비트 논리 연산자
+		System.out.println(27 & 13);
+		System.out.println(27 | 13);
 
 	}
 
