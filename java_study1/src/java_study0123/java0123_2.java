5b5f9f1559d67fa046dc8d4f5dafb142b1b30f83
tlssunw <sunw1044@naver.com>
2025-01-24 11:44:17 +0900

    비트논리연산자/비트쉬프트연산자

--- java_study1/src/java_study0123/java0123_2.java
+++ java_study1/src/java_study0123/java0123_2.java
@@ -5,8 +5,16 @@ public class java0123_2 {
 	public static void main(String[] args) {
 		
 		//비트 논리 연산자
-		System.out.println(27 & 13);
-		System.out.println(27 | 13);
+		System.out.println(27 & 13); // and연산
+		System.out.println(27 | 13); // or연산
+		System.out.println(27 ^ 13); //xor연산
+		System.out.println(~13); // not 연산
+		
+		//비트 쉬프트연산자
+		System.out.println(21 << 2); 
+		
+		// <<시프트 연산자
+		// 이동시켜주는 연산자(왼쪽으로 두 칸씩 이동)
 
 	}
 
