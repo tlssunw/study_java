1e518c64192afa2a8095ce699f6594aa7822cf94
tlssunw <sunw1044@naver.com>
2025-02-18 12:48:18 +0900

    0218 class

---
+++ java_class1/src/study0218/ClassArray.java
@@ -0,0 +1,42 @@
+
+package study0218;
+
+public class ClassArray {
+
+	public static void main(String[] args) {
+
+//		Member mem = new Member();
+//		mem.name="김유신";
+//		mem.job="군인";
+//		mem.age=38;
+		
+		
+		Member[] members = new Member[5];
+		// 5명 정보 저장 가능 ....하지만?
+//		for(int i=0; i<members.length; i++)
+//			members[i] = new Member();
+		
+//		members[0].job="감찰관";
+//		members[0].name="박문수";
+//		members[0].age=34;
+//		members[1].job="군인";
+//		members[1].name="이순신";
+//		members[1].age=31;
+//		members[2].job="문익점";
+//		members[2].name="산업스파이";
+//		members[2].age=45;
+//		
+//		for(int i=0; i<members.length; i++) {
+//			System.out.println(members[i].name);
+//			System.out.println(members[i].job);
+//			System.out.println(members[i].age);
+//		}
+		
+		Member info = new Member();
+
+		
+		
+		
+	}
+
+}
---
+++ java_class1/src/study0218/Member.java
@@ -0,0 +1,14 @@
+package study0218;
+
+public class Member {
+	
+	String name; // 이름
+	int age; // 나이
+	String job; // 직업
+	
+	Member(){
+		System.out.println("나생성자야!");
+	}
+	// 생성자 메소드
+
+}
